441d8f5fc4fed69950f8b62d19d58e3f9a4b4da9
0Z3R0 <49484081+i0Z3R0@users.noreply.github.com>
2023-01-08 22:24:50 -0500

    initial

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bref-Shorthand-Converter
+
+### Simple tool to convert normal text to Bref shorthand (more info [here](https://www.reddit.com/r/shorthand/comments/esjhdk/bref_shorthand/), manual [here](https://drive.google.com/drive/folders/1PZcAYhusYGpaLHwMUBAdZURA25lKk2Mu))
+
+### Usage
---
+++ main.py
@@ -0,0 +1,24 @@
+import csv
+
+abbr_dict = {}
+with open('abbr.csv', 'r') as csv_file:
+    csv_reader = csv.reader(csv_file)
+    for row in csv_reader:
+        abbr_dict[row[1]] = row[0]
+
+while True:
+    input_text = input("Enter some text (type 'q' to quit): ")
+    if input_text == "q":
+        print("Goodbye!")
+        break
+    output_words = []
+    chunks = input_text.split(" ")
+    for chunk in chunks:
+        if chunk in abbr_dict:
+            output_words.append(abbr_dict[chunk])
+        else:
+            output_words.append(chunk)
+    output_text = " ".join(output_words)
+    print(output_text)
+    with open("abbr.txt", "a") as output_file:
+        output_file.write(output_text + "\n\n")
